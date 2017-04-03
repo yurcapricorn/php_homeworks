a1b1854ca751dd51e638f8acd62a3349c9d8f915
Yury Odnorobov <yurcapricorn@gmail.com>
2017-04-03 11:59:22 +0300

    fixed errors (| changet to ||; removed includes; removed error displays)

--- hw1/article.php
+++ hw1/article.php
@@ -5,15 +5,13 @@ include_once __DIR__ . '/Models/Article.php';
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 } else {
-    echo 'incorrect request' . '<br><br>';
-    include_once __DIR__ . '/index.php';
-    die();
+    $error = 'incorrect request';
+    file_put_contents(__DIR__ . '/../errors.php', $error);
 }
 $article = \Models\Article::findById($id);
-if (false === $article | empty($article)) {
-    echo 'Article ' . $id . ' not found' . '<br><br>';
-    include_once __DIR__ . '/index.php';
-    die();
+if (false === $article || empty($article)) {
+    $error = 'Article ' . $id . ' not found';
+    file_put_contents(__DIR__ . '/errors.php', $error);
 }
 
 include __DIR__ . '/templates/article.html';
