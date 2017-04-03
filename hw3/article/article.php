a6f0f0d37a8b4b1cf469b82b307830d5795d0baa
Yury Odnorobov <yurcapricorn@gmail.com>
2017-04-03 11:50:14 +0300

    mistakes from hw1 fixed

--- hw3/article/article.php
+++ hw3/article/article.php
@@ -5,15 +5,13 @@ require_once __DIR__ . '/../protected/autoload.php';
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 } else {
-    echo 'incorrect request' . '<br><br>';
-    include_once __DIR__ . '/../index.php';
-    die();
+    $error = 'incorrect request' . '<br><br>';
+    file_put_contents(__DIR__ . '/../errors.php', $error);
 }
 $article = App\Models\Article::findById($id);
-if (false === $article | empty($article)) {
-    echo 'Article ' . $id . ' not found' . '<br><br>';
-    include_once __DIR__ . '/../index.php';
-    die();
+if (false === $article || empty($article)) {
+    $error = 'Article ' . $id . ' not found' . '<br><br>';
+    file_put_contents(__DIR__ . '/../errors.php', $error);
 }
 
 include __DIR__ . '/templates/article.html';
