112a2766286442fad2cdd8f67111f93f1b5a4c69
Yury Odnorobov <yurcapricorn@gmail.com>
2017-04-05 10:25:22 +0300

    errors management removed

--- hw2/admin/edit.php
+++ hw2/admin/edit.php
@@ -3,16 +3,16 @@
 include_once __DIR__ . '/../protected/App/Models/Article.php';
 
 if (empty($_POST['id'])) {
-    $error = 'no id specified';
+//    $error = 'no id specified';
 } else {
     $article = \App\Models\Article::findById($_POST['id']);
     if (($article === false) || empty($article)) {
-        $error = 'no such article';
+//        $error = 'no such article';
     } else {
         $title = $_POST['title'];
         $lead = $_POST['lead'];
         if (empty($title) && empty($lead)) {
-            $error = 'no data to update';
+//            $error = 'no data to update';
         } else {
             if (!empty($title)) {
                 $article->title = $title;
@@ -22,7 +22,7 @@ if (empty($_POST['id'])) {
             }
             $res = $article->save();
             if ($res === false) {
-                $error = 'save to db error';
+//                $error = 'save to db error';
             }
         }
     }
--- hw2/admin/remove.php
+++ hw2/admin/remove.php
@@ -3,15 +3,15 @@
 include_once __DIR__ . '/../protected/App/Models/Article.php';
 
 if (empty($_POST['id'])) {
-    $error = 'no id specified';
+//    $error = 'no id specified';
 } else {
     $article = \App\Models\Article::findById($_POST['id']);
     if (($article === false) || empty($article)) {
-        $error = 'no such article';
+//        $error = 'no such article';
     } else {
         $res = $article->delete();
         if ($res === false) {
-            $error = 'remove from db error';
+//            $error = 'remove from db error';
         }
     }
 }
