c089215d692b9c194d532ca6416e2ae673adac74
Yury Odnorobov <yurcapricorn@gmail.com>
2017-04-06 13:50:49 +0300

    cleanups

--- hw1/index.php
+++ hw1/index.php
@@ -4,10 +4,7 @@ require_once __DIR__ . '/autoload.php';
 
 $data = Models\Article::findLastEntries();
 
-if(empty($data) || $data === false) {
-//    $error = 'no records';
-} else {
-    include __DIR__ . '/templates/index.html';
-}
+include __DIR__ . '/templates/index.html';
+
 
 
