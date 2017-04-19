0bc61e5b64486f30a130dc8e2a17db343fcc38a5
Yury Odnorobov <yurcapricorn@gmail.com>
2017-04-19 18:37:26 +0300

    h\w7_v1.0

---
+++ hw7/protected/App/functions.php
@@ -0,0 +1,19 @@
+<?php
+use App\Models\Article;
+
+return [
+    function (Article $article) {
+        return $article->id;
+    },
+    function (Article $article) {
+        return $article->title;
+    },
+    function (Article $article) {
+        return $article->lead;
+    },
+    function (Article $article) {
+        if ($article->author_id !== NULL) {
+            return ($article->author->name . ' ' . $article->author->surname);
+        }
+    }
+];
