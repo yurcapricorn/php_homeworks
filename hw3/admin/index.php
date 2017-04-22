96ad131c4d0e2e69a62e029493d9a565bbbf00d7
Yury Odnorobov <yurcapricorn@gmail.com>
2017-04-22 13:57:56 +0300

    cleanups

--- hw3/admin/index.php
+++ hw3/admin/index.php
@@ -3,6 +3,6 @@
 require_once __DIR__ . '/../protected/autoload.php';
 
 $view = new App\View();
-$template = __DIR__ . '/templates/index.html';
+$template = __DIR__ . '/templates/admin.html';
 $view->articles = \App\Models\Article::findAll();
 $view->display($template);
